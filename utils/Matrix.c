aec7a3db7249af0593dcd55d128d0d9f0c5333f7
wei-huan <1520799054@qq.com>
2021-09-09 20:57:12 +0800

    添加初等变换函数

--- utils/Matrix.c
+++ utils/Matrix.c
@@ -1,25 +1,25 @@
 #include "Matrix.h"
 
 //row行, col列
-void Visit_Matrix(Matrix_Type A, int row, int col){
-    for(int i = 0; i < row; i++){
-        for(int j = 0; j < col; j++)
+void Visit_Matrix(Matrix_Type A, unsigned int row, unsigned int col){
+    for(unsigned int i = 0; i < row; i++){
+        for(unsigned int j = 0; j < col; j++)
             printf("%.2f ", ele(A, col, i, j));
         EOL;
     }
 }
 
 // 初始化row行, col列矩阵为零矩阵
-void Init_Matrix_Zero(Matrix_Type A, int row, int col){
-    for(int i = 0; i < row; i++)
-        for(int j = 0; j < col; j++)
+void Init_Matrix_Zero(Matrix_Type A, unsigned int row, unsigned int col){
+    for(unsigned int i = 0; i < row; i++)
+        for(unsigned int j = 0; j < col; j++)
             ele(A, col, i, j) = 0;
 }
 
 // 初始化方阵为单位矩阵
-void Init_Matrix_Identity(Matrix_Type A, int m){
-    for(int i = 0; i < m; i++)
-        for(int j = 0; j < m; j++)
+void Init_Matrix_Identity(Matrix_Type A, unsigned int m){
+    for(unsigned int i = 0; i < m; i++)
+        for(unsigned int j = 0; j < m; j++)
             if(i == j)
                 ele(A, m, i, j) = 1;
             else
@@ -27,58 +27,58 @@ void Init_Matrix_Identity(Matrix_Type A, int m){
 }
 
 // 复制矩阵 A 给 B, row行, col列
-void Copy_Matrix(Matrix_Type A, Matrix_Type B, int row, int col){
-    for(int i = 0; i < row; i++)
-        for(int j = 0; j < col; j++)
+void Copy_Matrix(Matrix_Type A, Matrix_Type B, unsigned int row, unsigned int col){
+    for(unsigned int i = 0; i < row; i++)
+        for(unsigned int j = 0; j < col; j++)
             ele(B, col, i, j) = ele(A, col, i, j);
 }
 
 // C = A + B, row行, col列
-void Matrix_Addition(Matrix_Type A, Matrix_Type B, Matrix_Type C, int row, int col){
-    for(int i = 0; i < row; i++)
-        for(int j = 0; j < col; j++)
+void Matrix_Addition(Matrix_Type A, Matrix_Type B, Matrix_Type C, unsigned int row, unsigned int col){
+    for(unsigned int i = 0; i < row; i++)
+        for(unsigned int j = 0; j < col; j++)
             ele(C, col, i, j) = ele(A, col, i, j) + ele(B, col, i, j);
 }
 
 // C = A - B, row行, col列
-void Matrix_Subtraction(Matrix_Type A, Matrix_Type B, Matrix_Type C, int row, int col){
+void Matrix_Subtraction(Matrix_Type A, Matrix_Type B, Matrix_Type C, unsigned int row, unsigned int col){
 
-    for(int i = 0; i < row; i++)
-        for(int j = 0; j < col; j++)
+    for(unsigned int i = 0; i < row; i++)
+        for(unsigned int j = 0; j < col; j++)
             ele(C, col, i, j) = ele(A, col, i, j) - ele(B, col, i, j);
 }
 
 //C = A * B, A在左, B在右, Amt, Btn
-void Matrix_Multiplication(Matrix_Type A, Matrix_Type B, Matrix_Type C, int m, int t, int n){
+void Matrix_Multiplication(Matrix_Type A, Matrix_Type B, Matrix_Type C, unsigned int row1, unsigned int col1_row2, unsigned int col2){
     Matrix_datatype tmp;
 
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
+    for(unsigned int i = 0; i < row1; i++){
+        for(unsigned int j = 0; j < col2; j++){
             tmp = (Matrix_datatype)0;
 
-            for(int k = 0; k < t; k++)
-                tmp += ele(A, t, i, k) * ele(B, n, k, j);
+            for(unsigned int k = 0; k < col1_row2; k++)
+                tmp += ele(A, col1_row2, i, k) * ele(B, col2, k, j);
 
-            ele(C, n, i, j) = tmp;
+            ele(C, col2, i, j) = tmp;
         }
     }
 }
 
 // B = A-1 A为m阶方阵, B = A-1 = A* / detA
-void Matrix_Inverse_Adjoint_Matrix(Matrix_Type A, Matrix_Type B, int m){
+void Matrix_Inverse_Adjoint_Matrix(Matrix_Type A, Matrix_Type B, unsigned int m){
     Matrix_datatype detA = 0.0;
 
     // 求 A 的伴随矩阵
-    for(int i = 0; i < m; i++)
-        for(int j = 0; j < m; j++)
+    for(unsigned int i = 0; i < m; i++)
+        for(unsigned int j = 0; j < m; j++)
             ele(B, m, i, j) = pow(-1, i + j) * Matrix_Cofactor(A, m, j, i);
         
     // Visit_Matrix(B, m, m);
 
     // 求 A 的行列式 detA
-    for(int j = 0; j < m; j++)
+    for(unsigned int j = 0; j < m; j++)
         detA += ele(A, m, 0, j) * ele(B, m, 0, j);
-    // printf("detA: %.2f\n", detA);
+    printf("detA: %.2f\n", detA);
 
     //detA == 0说明矩阵不可逆
     if(detA == 0){
@@ -87,20 +87,20 @@ void Matrix_Inverse_Adjoint_Matrix(Matrix_Type A, Matrix_Type B, int m){
     }
     
     // 逆矩阵 B
-    for(int i = 0; i < m; i++)
-        for(int j = 0; j < m; j++)
+    for(unsigned int i = 0; i < m; i++)
+        for(unsigned int j = 0; j < m; j++)
             ele(B, m, i, j)= ele(B, m, i, j) / detA;
 }
 
 // 高斯约当消元法求A的逆矩阵B
-void Matrix_Inverse_Gauss_Jordan(Matrix_Type A, Matrix_Type B, int m){
+void Matrix_Inverse_Gauss_Jordan(Matrix_Type A, Matrix_Type B, unsigned int m){
     Init_Matrix_Identity(B, m);
     
-    //
+    // 
 }
 
 // 求矩阵关于某一个元素aij的代数余子式, 即二维数组矩阵A下标为ij的元素的代数余子式
-Matrix_datatype Matrix_Cofactor(Matrix_Type A, int m, int i, int j){
+Matrix_datatype Matrix_Cofactor(Matrix_Type A, unsigned int m, unsigned int i, unsigned int j){
     if(m == 1){
         printf("矩阵小于2阶\n");
         return 0;
@@ -113,10 +113,10 @@ Matrix_datatype Matrix_Cofactor(Matrix_Type A, int m, int i, int j){
 
     if(m == 3){
         Matrix_datatype B[2][2];
-        int a = 0, b = 0;
+        unsigned int a = 0, b = 0;
 
-        for(int x = 0; x < m; x++){
-            for(int y = 0; y < m; y++)
+        for(unsigned int x = 0; x < m; x++){
+            for(unsigned int y = 0; y < m; y++)
                 if((x != i) && (y != j))
                     B[a][b++] = ele(A, m, x, y);
 
@@ -132,15 +132,15 @@ Matrix_datatype Matrix_Cofactor(Matrix_Type A, int m, int i, int j){
     }
 
     Matrix_datatype tmp1, tmp2;
-    int count_p, x_jump_p, y_jump_p;
-    int count_n, x_jump_n, y_jump_n;
+    unsigned int count_p, x_jump_p, y_jump_p;
+    unsigned int count_n, x_jump_n, y_jump_n;
 
-    for(int x = 0; x < m; x++){
+    for(unsigned int x = 0; x < m; x++){
         tmp1 = tmp2 = (Matrix_datatype)1;
         count_p = count_n = x_jump_p = y_jump_p = x_jump_n = y_jump_n = 0;
 
         if(x != i){
-            for(int y = 0; y < m; y++){
+            for(unsigned int y = 0; y < m; y++){
                 if(y == j)
                     y_jump_p = 1;
 
@@ -173,9 +173,68 @@ Matrix_datatype Matrix_Cofactor(Matrix_Type A, int m, int i, int j){
 }
 
 // B = AT
-void Matrix_Transpose(Matrix_Type A, Matrix_Type B, int row, int col){
-    for(int i = 0; i < row; i++)
-        for(int j = 0; j < col; j++)
+void Matrix_Transpose(Matrix_Type A, Matrix_Type B, unsigned int row, unsigned int col){
+    for(unsigned int i = 0; i < row; i++)
+        for(unsigned int j = 0; j < col; j++)
             ele(B, row, j, i) = ele(A, col, i, j);
 }
 
+// 行初等变换, 交换两行元素
+void Matrix_Row_Switch(Matrix_Type A, unsigned int row, unsigned int col, unsigned int i1, unsigned int i2){
+    Matrix_datatype switch_aid;
+
+    // 检查输入行数是否在范围内
+    if(i1 >= row || i2 >= row){
+        printf("行数超出矩阵的行大小\n");
+        return ;
+    }
+
+    // 交换第row1和row2行
+    for(unsigned int j = 0; j < col; j++){
+        switch_aid = ele(A, col, i1, j);
+        ele(A, col, i1, j) = ele(A, col, i2, j);
+        ele(A, col, i2, j) = switch_aid;
+    }
+}
+
+// 列初等变换, 交换两列元素
+void Matrix_Column_Switch(Matrix_Type A, unsigned int row, unsigned int col, unsigned int j1, unsigned int j2){
+    Matrix_datatype switch_aid;
+
+    // 检查输入列数是否在范围内
+    if(j1 >= col || j2 >= col){
+        printf("列数超出矩阵的列大小\n");
+        return ;
+    }
+
+    // 交换第row1和row2行
+    for(unsigned int i = 0; i < col; i++){
+        switch_aid = ele(A, col, i, j1);
+        ele(A, col, i, j1) = ele(A, col, i, j2);
+        ele(A, col, i, j2) = switch_aid;
+    }
+}
+
+// 行初等变换, 第i行元乘非零数
+void Matrix_Row_Multiplication(Matrix_Type A, Matrix_datatype multiplier, unsigned int row, unsigned int col, unsigned int i){
+    // 检查输入行数是否在范围内
+    if(i >= row){
+        printf("行数超出矩阵的行大小\n");
+        return ;
+    }
+
+    for(unsigned int j = 0; j < col; j++)
+        ele(A, col, i, j) = multiplier * ele(A, col, i, j);
+}
+
+// 列初等变换, 第j列元乘非零数
+void Matrix_Column_Multiplication(Matrix_Type A, Matrix_datatype multiplier, unsigned int row, unsigned int col, unsigned int j){
+    // 检查输入列数是否在范围内
+    if(j >= col){
+        printf("列数超出矩阵的列大小\n");
+        return ;
+    }
+
+    for(unsigned int i = 0; i < row; i++)
+        ele(A, col, i, j) = multiplier * ele(A, col, i, j);
+}
--- utils/Matrix.h
+++ utils/Matrix.h
@@ -15,15 +15,19 @@ typedef Matrix_Type* Matrix_P_Type;
 #define ele(A, col, i, j)     (*(((Matrix_Row_Type)A) + (col) * (i) + (j)))
 #define EOL                   (putchar('\n'))
 
-void Init_Matrix_Zero(Matrix_Type A, int row, int col);
-void Init_Matrix_Identity(Matrix_Type A, int m);
-void Copy_Matrix(Matrix_Type A, Matrix_Type B, int row, int col);
-void Matrix_Addition(Matrix_Type A, Matrix_Type B, Matrix_Type C, int row, int col);
-void Matrix_Subtraction(Matrix_Type A, Matrix_Type B, Matrix_Type C, int row, int col);
-void Matrix_Multiplication(Matrix_Type A, Matrix_Type B, Matrix_Type C, int row1, int col1_row2, int col2);
-void Matrix_Inverse_Adjoint_Matrix(Matrix_Type A, Matrix_Type B, int m);
-void Matrix_Inverse_Gauss_Jordan(Matrix_Type A, Matrix_Type B, int m);
-void Visit_Matrix(Matrix_Type A, int row, int col);
-Matrix_datatype Matrix_Cofactor(Matrix_Type A, int m, int i, int j);
-void Matrix_Transpose(Matrix_Type A, Matrix_Type B, int row, int col);
+void Init_Matrix_Zero(Matrix_Type A, unsigned int row, unsigned int col);
+void Init_Matrix_Identity(Matrix_Type A, unsigned int m);
+void Copy_Matrix(Matrix_Type A, Matrix_Type B, unsigned int row, unsigned int col);
+void Matrix_Addition(Matrix_Type A, Matrix_Type B, Matrix_Type C, unsigned int row, unsigned int col);
+void Matrix_Subtraction(Matrix_Type A, Matrix_Type B, Matrix_Type C, unsigned int row, unsigned int col);
+void Matrix_Multiplication(Matrix_Type A, Matrix_Type B, Matrix_Type C, unsigned int row1, unsigned int col1_row2, unsigned int col2);
+void Matrix_Inverse_Adjoint_Matrix(Matrix_Type A, Matrix_Type B, unsigned int m);
+void Matrix_Inverse_Gauss_Jordan(Matrix_Type A, Matrix_Type B, unsigned int m);
+void Visit_Matrix(Matrix_Type A, unsigned int row, unsigned int col);
+Matrix_datatype Matrix_Cofactor(Matrix_Type A, unsigned int m, unsigned int i, unsigned int j);
+void Matrix_Transpose(Matrix_Type A, Matrix_Type B, unsigned int row, unsigned int col);
+void Matrix_Row_Switch(Matrix_Type A, unsigned int row, unsigned int col, unsigned int row1, unsigned int row2);
+void Matrix_Column_Switch(Matrix_Type A, unsigned int row, unsigned int col, unsigned int col1, unsigned int col2);
+void Matrix_Row_Multiplication(Matrix_Type A, Matrix_datatype multiplier, unsigned int row, unsigned int col, unsigned int i);
+void Matrix_Column_Multiplication(Matrix_Type A, Matrix_datatype multiplier, unsigned int row, unsigned int col, unsigned int j);
 #endif /*MATRIX_H*/
